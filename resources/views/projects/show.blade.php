879888c7ded5b04c0811d12784e09276a30cb29b
Andy Hollandmoritz <ahollandmoritz@gmail.com>
2023-06-15 05:41:56 -0400

    I like the squares idea... takes a second to know what they are and then you're good

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -4,24 +4,54 @@
 
         <div>
             <h2>Boards</h2>
-            <div>
-                @foreach($project->boards as $board)
-                    <div id="board-list-{{ $board->id }}" class="flex items-center p-1">
-                        {{-- We could make it expandable... or open an off-canvas, or we can focus on deep-linking /projects/board/tasks --}}
-                        <x-icon icon="caret-right" class="h-3 w-3 mr-1" />
-                        <a class="link" href="#">{{ $board->name }}</a>
-                        <div class="text-xs ml-4 flex items-center space-x-3">
-                            <span title="Incomplete">{{ $board->incomplete_tasks_count }}</span>
-                            @if($board->past_due_tasks_count > 0)
-                                <span title="Past Due Tasks" class="text-danger">{{ $board->past_due_tasks_count }}</span>
-                            @endif
-                            @if($board->incomplete_user_tasks_count > 0)
-                                <span title="Your Incomplete tasks" class="text-primary">{{ $board->incomplete_user_tasks_count }}</span>
-                            @endif
-                        </div>
+            <div class="row">
+                <div class="col-span-12 project-board-menu">
+                    @foreach($project->boards as $board)
+                        @php
+                            $class = '';
+                            if($board->incomplete_tasks_count > 0) {
+                                $class .= 'has-incomplete';
+                            }
 
-                    </div>
-                @endforeach
+                            if($board->past_due_tasks_count > 0) {
+                                $class .= 'has-past-due';
+                            }
+
+                            if($board->incomplete_user_tasks_count > 0) {
+                                $class .= 'has-incomplete-user';
+                            }
+                        @endphp
+                        <a
+                            href="#"
+                            id="board-list-{{ $board->id }}"
+                            class="p-1  flex text-sm rounded-md hover:bg-zinc-300"
+                            data-active-class="active"
+                            title="{{ $board->name }}"
+                        >
+
+                            <span class="grid grid-cols-3 items-center text-xs gap-0 max-w-[70px] min-w-[70px]">
+                                <span title="All Incomplete tasks" class="border text-center truncate {{ $board->incomplete_tasks_count > 0 ? 'bg-zinc-600 text-white' : '' }}">
+                                    {{ $board->incomplete_tasks_count }}
+
+                                </span>
+                                <span title="Past Due Tasks" class="border text-center truncate {{ $board->past_due_tasks_count > 0 ? 'bg-red-600 text-white' : '' }}">
+                                    {{ $board->past_due_tasks_count }}
+                                </span>
+                                <span title="Your incomplete tasks" class="border text-center truncate {{ $board->incomplete_user_tasks_count > 0 ? 'bg-blue-600 text-white' : '' }}">
+                                    {{ $board->incomplete_user_tasks_count }}
+                                </span>
+                            </span>
+                            <span class="link ml-2 truncate">
+                                {{ $board->name }}
+                            </span>
+
+                        </a>
+                    @endforeach
+                </div>
+                <div class="col-span-9">
+                    BOARD DETAIL GOES HERE
+                </div>
+
             </div>
         </div>
 
