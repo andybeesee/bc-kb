4439f6da64bdd42b5580f4811164b63034daad03
ahollandmoritz <ahollandmoritz@glastender.com>
2023-06-29 11:48:45 -0400

    index looks good

--- resources/views/admin/teams/index.blade.php
+++ resources/views/admin/teams/index.blade.php
@@ -7,12 +7,12 @@
 
         <div class="list-group">
             @foreach($teams as $team)
-                <div class="list-group-item">
+                <div class="list-group-item hover:bg-zinc-50">
                     <a class="link" href="{{ route('admin.teams.show', $team) }}">
                         {{ $team->name }}
                     </a>
 
-                    <small>{{ $team->members_count }} members</small>
+                    <span class="text-sm block md:ml-2 md:inline">{{ $team->members_count }} members</span>
 
                 </div>
             @endforeach
