0258b122521cdebb42ab8278c2badf16d85decc9
Andy Hollandmoritz <ahollandmoritz@gmail.com>
2023-07-05 05:20:49 -0400

    added TODOs

--- resources/views/livewire/project-task-list.blade.php
+++ resources/views/livewire/project-task-list.blade.php
@@ -11,7 +11,6 @@
         </div>
     </div>
 
-
     <div class="my-3">
         <div>
             <button class="btn btn-white" x-show="!addingGroup" type="button" @click="addingGroup = true">
@@ -30,18 +29,19 @@
                     <button type="submit" class="btn btn-primary">Add Group</button>
                     <button type="button" class="btn btn-white" @click="addingGroup = false">Cancel</button>
                 </div>
-
             </form>
-
         </div>
     </div>
 
+    {{-- TODO: Group Sorting --}}
+    {{-- TODO: Moving tasks between groups --}}
 
     @foreach($groups as $group)
+        {{-- TODO: Dropdown to change color of section --}}
         <div class="mt-4 grid gap-4">
             <div class="card">
                 <div class="card-title flex items-center">
-                    <div class="mr-1 cursor-move">
+                    <div class="mr-1 cursor-move handle">
                         <x-icon icon="grip-vertical" class="h-4 w-4" />
                     </div>
                     {{ $group->name }}
@@ -55,6 +55,4 @@
             </div>
         </div>
     @endforeach
-
-    {{-- Be like water. --}}
 </div>
