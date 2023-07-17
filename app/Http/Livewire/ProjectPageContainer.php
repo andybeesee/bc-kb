4803facd6d4234ffb66ee38ea4ecb3b084952a19
Andy Hollandmoritz <ahollandmoritz@gmail.com>
2023-07-17 05:04:43 -0400

    added our simple, not great dashboard

--- app/Http/Livewire/ProjectPageContainer.php
+++ app/Http/Livewire/ProjectPageContainer.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Project;
+use App\Models\Task;
 use Livewire\Component;
 
 class ProjectPageContainer extends Component
@@ -27,7 +28,13 @@ class ProjectPageContainer extends Component
 
     public function render()
     {
-        return view('livewire.project-page-container');
+        $dashboardData = [];
+        if(empty($this->projectId)) {
+            $dashboardData = $this->getDashboardData();
+        }
+
+        return view('livewire.project-page-container')
+            ->with('dashboardData', $dashboardData);
     }
 
     public function closeProject()
@@ -57,5 +64,45 @@ class ProjectPageContainer extends Component
         }
 
         return $q->limit(200)->get();
+    }
+
+    protected function getDashboardData()
+    {
+        $data = [];
+        $data['currentProjects'] = Project::with('team')
+            ->whereIn('status', [
+                'planning',
+                'planned',
+                'in_progress',
+                'late',
+            ])
+            ->where('owner_id', auth()->user()->id)
+            ->get();
+
+        $data['pastDueTasks'] = Task::incomplete()
+            ->with('project')
+            ->where('due_date', '<', date('Y-m-d'))
+            ->where('assigned_to', auth()->user()->id)
+            ->orderBy('due_date', 'ASC')
+            ->get();
+
+        $data['upcomingDueTasks'] = Task::incomplete()
+            ->with('project')
+            ->whereNotNull('due_date')
+            ->whereBetween('due_date', [
+                date('Y-m-d'),
+                date('Y-m-d', strtotime('+2 weeks'))
+            ])
+            ->orderBy('due_date', 'ASC')
+            ->where('assigned_to', auth()->user()->id)
+            ->get();
+
+        $data['incompleteTasks'] = Task::incomplete()
+            ->with('project')
+            ->whereNull('due_date')
+            ->where('assigned_to', auth()->user()->id)
+            ->get();
+
+        return $data;
     }
 }
--- resources/views/livewire/project-page-container.blade.php
+++ resources/views/livewire/project-page-container.blade.php
@@ -10,21 +10,83 @@
             />
         </div>
         <div class="grid">
+            <button
+                wire:click.self="$set('projectId', null)"
+                class="cursor-pointer text-sm p-2 flex truncate items-center rounded-md {{ empty($projectId) ? 'bg-purple-100 dark:bg-purple-700 hover:bg-purple-200 dark:hover:bg-purple-800' : 'hover:bg-zinc-100 hover:bg-zinc-700' }}"
+                title="Show dashboard"
+            >
+                Dashboard
+            </button>
             @foreach($this->projects as $project)
-                <div
+                <button type="button"
                     wire:click.self="$set('projectId', {{$project->id}})"
                     class="cursor-pointer text-sm p-2 flex truncate items-center rounded-md {{ $project->id === $projectId ? 'bg-purple-100 dark:bg-purple-700 hover:bg-purple-200 dark:hover:bg-purple-800' : 'hover:bg-zinc-100 hover:bg-zinc-700' }}"
                     title="{{ $project->name }} -- {{ $project->status }}"
                 >
                     {{ $project->name }}
-                </div>
+                </button>
             @endforeach
         </div>
     </div>
     <div class="col-span-3">
         @if(empty($projectId))
-            <div>
-                Dashboard or somethign
+            <div class="grid md:grid-cols-2 items-start gap-4">
+                @if($dashboardData['currentProjects']->count() > 0)
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="font-bold text-lg mb-3">{{ $dashboardData['currentProjects']->count() }} Current Projects</div>
+                            <div class="grid">
+                                @foreach($dashboardData['currentProjects'] as $project)
+                                    <button wire:click="$set('projectId', {{ $project->id }})" type="button" class="text-left underline hover:text-blue-600">
+                                        {{ $project->name }}
+                                    </button>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                @if($dashboardData['pastDueTasks']->count() > 0)
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="font-bold text-lg text-red-700 dark:text-red-400 mb-3">{{ $dashboardData['pastDueTasks']->count() }} Past Due Tasks</div>
+                            <div class="grid">
+                                @foreach($dashboardData['pastDueTasks'] as $task)
+                                    <button wire:click="$set('projectId', {{ $task->project_id }})" type="button" class="text-left hover:text-blue-600">
+                                        {{ $task->name }} <small>({{ $task->project->name }})</small>
+                                    </button>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                @if($dashboardData['upcomingDueTasks']->count() > 0)
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="font-bold text-lg mb-3">{{ $dashboardData['upcomingDueTasks']->count() }} Upcoming Tasks</div>
+                            <div class="grid">
+                                @foreach($dashboardData['upcomingDueTasks'] as $task)
+                                    <button wire:click="$set('projectId', {{ $task->project_id }})" type="button" class="text-left hover:text-blue-600">
+                                        {{ $task->name }} <small>({{ $task->project->name }})</small>
+                                    </button>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                @if($dashboardData['incompleteTasks']->count() > 0)
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="font-bold text-lg mb-3">{{ $dashboardData['incompleteTasks']->count() }} Incomplete Tasks</div>
+                            <div class="grid">
+                                @foreach($dashboardData['incompleteTasks'] as $task)
+                                    <button wire:click="$set('projectId', {{ $task->project_id }})" type="button" class="text-left hover:text-blue-600">
+                                        {{ $task->name }} <small>({{ $task->project->name }})</small>
+                                    </button>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endif
             </div>
         @else
             <livewire:project-detail-page :project-id="$projectId" wire:key="container-project-detail-{{ $projectId }}" />
