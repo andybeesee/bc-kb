98bd09789d942ab9d12913b8473114d3b2165002
ahollandmoritz <ahollandmoritz@glastender.com>
2023-07-21 06:21:36 -0400

    project index looking even better

--- resources/views/livewire/project-index-page.blade.php
+++ resources/views/livewire/project-index-page.blade.php
@@ -51,7 +51,7 @@
                     <div class="grid grid-cols-4 gap-6 items-start">
                         <div>
                             <div class="mb-4 grid">
-                                <div class="font-semibold text-sm">Statuses to Show</div>
+                                <div class="font-semibold mb-1 text-sm">Statuses to Show</div>
                                 @foreach($statusOptions as $statusValue => $statusName)
                                     <label class="py-1">
                                         <input
@@ -64,77 +64,75 @@
                                     </label>
                                 @endforeach
                             </div>
-                            <div class="font-semibold text-sm mb-2">Projects to Show</div>
+                            <div class="font-semibold text-sm mb-1">Projects to Show</div>
                             <div>
-                                <div class="bg-zinc-100 dark:bg-zinc-700  text-sm" x-show="open">
-                                    <button type="button" class="w-full text-left p-2 {{ $projectList === 'user' && $userId === auth()->user()->id ? 'bg-purple-200 dark:bg-purple-700' : '' }}" wire:click="showUserProjects({{ auth()->user()->id }})">
-                                        Your Projects
+                                <button type="button" class="w-full text-left rounded-md p-1 {{ $projectList === 'user' && $userId === auth()->user()->id ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-300 dark:hover:bg-zinc-900' }}" wire:click="showUserProjects({{ auth()->user()->id }})">
+                                    Your Projects
+                                </button>
+
+                                <button type="button" class="w-full text-left rounded-md p-1 {{ $projectList === 'team' && empty($teamId) ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-300 dark:hover:bg-zinc-900' }}" wire:click="showTeamProjects(null)">
+                                    Projects From Your Teams
+                                </button>
+
+                                <div x-data="{ open: false }">
+                                    <button class="text-left p-1 flex items-center rounded-md flex w-full hover:bg-zinc-300 dark:hover:bg-zinc-900" type="button" @click="open = !open">
+                                        <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
+                                        <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
+                                        Your Teams
                                     </button>
+                                    <div class="grid border-l pl-3 ml-4 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
+                                        @foreach($userTeams as $uTeam)
+                                            <button class="text-left p-1 rounded {{ $uTeam->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$uTeam->id}})">
+                                                {{ $uTeam->name }}
+                                            </button>
+                                        @endforeach
+                                    </div>
+                                </div>
 
-                                    <button type="button" class="w-full text-left p-2 {{ $projectList === 'team' && empty($teamId) ? 'bg-purple-200 dark:bg-purple-700' : '' }}" wire:click="showTeamProjects(null)">
-                                        Projects From Your Teams
+                                <div x-data="{ open: false }">
+                                    <button class="text-left p-1 rounded-md flex w-full hover:bg-zinc-300 dark:hover:bg-zinc-900 items-center" type="button" @click="open = !open">
+                                        <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
+                                        <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
+                                        All Teams
                                     </button>
+                                    <div class="grid border-l pl-3 ml-4 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
+                                        @foreach($allTeams as $aTeam)
+                                            <button class="text-left p-1 rounded {{ $aTeam->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$aTeam->id}})">
+                                                {{ $aTeam->name }}
+                                            </button>
+                                        @endforeach
+                                    </div>
+                                </div>
 
-                                    <div x-data="{ open: false }">
-                                        <button class="text-left p-2 flex items-center" type="button" @click="open = !open">
-                                            <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
-                                            <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
-                                            Your Teams
-                                        </button>
-                                        <div class="grid border-l pl-3 ml-4 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
-                                            @foreach($userTeams as $uTeam)
-                                                <button class="text-left p-1 rounded {{ $uTeam->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$uTeam->id}})">
-                                                    {{ $uTeam->name }}
+                                <div x-data="{ open: false }">
+                                    <button class="text-left p-1 flex items-center rounded-md flex w-full hover:bg-zinc-300 dark:hover:bg-zinc-900" type="button" @click="open = !open">
+                                        <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
+                                        <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
+                                        Users on Your Teams
+                                    </button>
+                                    <div class="grid ml-4  border-l pl-3 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
+                                        @if($usersOnYourTeams->count() > 0)
+                                            @foreach($usersOnYourTeams as $teamUser)
+                                                <button class="text-left p-1 rounded {{ $teamUser->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$teamUser->id}})">
+                                                    {{ $teamUser->name }}
                                                 </button>
                                             @endforeach
-                                        </div>
+                                        @endif
                                     </div>
+                                </div>
 
-                                    <div x-data="{ open: false }">
-                                        <button class="text-left p-2 flex items-center" type="button" @click="open = !open">
-                                            <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
-                                            <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
-                                            All Teams
-                                        </button>
-                                        <div class="grid border-l pl-3 ml-4 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
-                                            @foreach($allTeams as $aTeam)
-                                                <button class="text-left p-1 rounded {{ $aTeam->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$aTeam->id}})">
-                                                    {{ $aTeam->name }}
-                                                </button>
-                                            @endforeach
-                                        </div>
-                                    </div>
-
-                                    <div x-data="{ open: false }">
-                                        <button class="text-left p-2 flex items-center" type="button" @click="open = !open">
-                                            <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
-                                            <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
-                                            Users on Your Teams
-                                        </button>
-                                        <div class="grid ml-4  border-l pl-3 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
-                                            @if($usersOnYourTeams->count() > 0)
-                                                @foreach($usersOnYourTeams as $teamUser)
-                                                    <button class="text-left p-1 rounded {{ $teamUser->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$teamUser->id}})">
-                                                        {{ $teamUser->name }}
-                                                    </button>
-                                                @endforeach
-                                            @endif
-                                        </div>
-                                    </div>
-
-                                    <div x-data="{ open: false }">
-                                        <button class="text-left p-2 flex items-center" type="button" @click="open = !open">
-                                            <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
-                                            <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
-                                            All Users
-                                        </button>
-                                        <div class="grid ml-4  border-l pl-3 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
-                                            @foreach($allUsers as $alluser)
-                                                <button class="text-left p-1 rounded {{ $alluser->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$alluser->id}})">
-                                                    {{ $alluser->name }}
-                                                </button>
-                                            @endforeach
-                                        </div>
+                                <div x-data="{ open: false }">
+                                    <button class="text-left p-1 flex items-center rounded-md flex w-full hover:bg-zinc-300 dark:hover:bg-zinc-900" type="button" @click="open = !open">
+                                        <x-icon icon="caret-right" x-show="!open" class="h-4 w-4 mr-1" />
+                                        <x-icon icon="caret-down" x-show="open" class="h-4 w-4 mr-1" />
+                                        All Users
+                                    </button>
+                                    <div class="grid ml-4  border-l pl-3 max-h-[350px] overflow-y-scroll" x-show="open" style="display: none;">
+                                        @foreach($allUsers as $alluser)
+                                            <button class="text-left p-1 rounded {{ $alluser->id === $teamId ? 'bg-purple-200 dark:bg-purple-700' : 'hover:bg-zinc-800' }}" type="button" wire:click="showTeamProjects({{$alluser->id}})">
+                                                {{ $alluser->name }}
+                                            </button>
+                                        @endforeach
                                     </div>
                                 </div>
                             </div>
