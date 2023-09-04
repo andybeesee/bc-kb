8f11cc3fb8ce27db70bab8bc75089baf5df302a2
Andy Hollandmoritz <ahollandmoritz@gmail.com>
2023-09-03 20:54:32 -0400

    dark colors on tabnav

--- resources/js/Components/TabNav.vue
+++ resources/js/Components/TabNav.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="p-1 bg-zinc-300 rounded-md space-x-1 flex justify-between">
+    <div class="p-1 bg-zinc-300 dark:bg-zinc-600 rounded-md space-x-1 flex justify-between">
         <button
             v-for="tab in tabs"
             type="button"
             :class="{
                 'p-1 flex-grow rounded-md text-center': true,
-                'bg-white font-semibold hover:bg-zinc-50': tab.id === active,
-                'hover:bg-zinc-50': tab.id !== active,
+                'bg-white dark:bg-zinc-900 font-semibold hover:bg-zinc-50 dark:hover:bg-zinc-700': tab.id === active,
+                'hover:bg-zinc-50 dark:hover:bg-zinc-700': tab.id !== active,
 
             }"
             @click="$emit('change', tab.id)"
